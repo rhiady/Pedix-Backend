66b4b30afdc40e64caa0df253fc44c022e7ac60d
Rhiady Vieira <rhiadyv1@gmail.com>
2026-01-26 22:20:24 -0300

    erros personalizados atualizada

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -3,6 +3,7 @@ package com.services;
 import com.entities.User;
 import com.repositories.UserRepository;
 import com.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity,user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity,user);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
     private void updateData(User entity, User user) {
         entity.setName(user.getName());
